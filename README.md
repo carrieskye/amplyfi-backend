f49e9bc192921a341c2efff4943775d5899e2b00
Carolyne Peelman <peelmancarolyne@gmail.com>
2019-05-13 12:44:01 +0100

    Docs and companies API

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# amplyfi-exercise
-Technical exercise for job interview at Amplyfi
+# Backend
+## Backend for technical exercise for job interview at Amplyfi
+
+
+
+Create virtual environment:
+
+```bash
+conda create -n amplyfi
+conda activate amplyfi
+conda install python
+```
+
+
+
+Install packages:
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ app.py
@@ -0,0 +1,116 @@
+import json
+
+import pymongo
+from bson import json_util
+from flask import Flask
+from flask_cors import CORS
+from flask_restful import Resource, Api, reqparse
+from pymongo import MongoClient
+
+# Setup
+user = 'amplyfi'
+pw = 'pg9sWc1hWkrI7nPBeSR5'
+
+client_amplyfi = MongoClient(f'mongodb://{user}:{pw}@ds155396.mlab.com:55396/amplyfi')
+db_amplyfi = client_amplyfi.amplyfi
+docs = db_amplyfi.docs
+
+client_amplyfi_relations = MongoClient(f'mongodb://{user}:{pw}@ds155606.mlab.com:55606/amplyfi-relations')
+db_amplyfi_relations = client_amplyfi_relations['amplyfi-relations']
+companies = db_amplyfi_relations.companies
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+api = Api(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+class Docs(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('limit', type=int)
+        parser.add_argument('skip', type=int)
+        args = parser.parse_args()
+        limit = args['limit'] if args['limit'] is not None else 20
+        skip = args['skip'] if args['skip'] is not None else 0
+        # return json.loads(json_util.dumps(docs.find().sort('m_szYear', pymongo.DESCENDING).limit(limit).skip(skip)))
+        return json.loads(json_util.dumps(docs.find().limit(limit).skip(skip)))
+
+
+class Doc(Resource):
+    def get(self, doc_id):
+        return get_document(doc_id)
+
+
+class DocsCount(Resource):
+    def get(self):
+        return json.loads(json_util.dumps(docs.count()))
+
+
+class Companies(Resource):
+    def get(self):
+        return json.loads(json_util.dumps(companies.find().sort("name", 1)))
+
+
+class Company(Resource):
+    def get(self, company_name):
+        company = json.loads(json_util.dumps(companies.find_one({"name": company_name})))
+        company_docs = json.loads(
+            json_util.dumps(docs.find({"m_Companies": {"$in": [company_name]}}).sort('m_szYear', pymongo.DESCENDING)))
+        company['docs'] = company_docs
+        return company
+
+
+class UpdateCompanies(Resource):
+    def get(self):
+        documents = json.loads(json_util.dumps(docs.find()))
+        for doc in documents:
+            for company in doc['m_Companies']:
+                if json.loads(json_util.dumps(companies.find_one({"name": company}))) is None:
+                    add_company(company, [doc['m_szDocID']], doc['m_szYear'], doc['m_szYear'])
+                else:
+                    update_company(company, [doc['m_szDocID']], doc['m_szYear'], doc['m_szYear'])
+
+        return json.loads(json_util.dumps(companies.find()))
+
+
+def get_document(id):
+    return json.loads(json_util.dumps(docs.find_one({"m_szDocID": id})))
+
+
+def add_company(name, docs, min_year, max_year):
+    company = {
+        "name": name,
+        "docs": docs,
+        "min_year": str(min_year),
+        "max_year": str(max_year)
+    }
+    companies.insert_one(company)
+    return company
+
+
+def update_company(name, new_docs, min_year, max_year):
+    company = json.loads(json_util.dumps(companies.find_one({"name": name})))
+
+    for doc in new_docs:
+        if doc not in company['docs']:
+            company['docs'].append(doc)
+
+    company['min_year'] = min_year if min_year < company['min_year'] else company['min_year']
+    company['max_year'] = max_year if max_year > company['max_year'] else company['max_year']
+
+    companies.update_one({'name': name},
+                         {"$set": {'docs': company['docs'], 'min_year': company['min_year'],
+                                   'max_year': company['max_year']}})
+
+
+api.add_resource(Docs, '/api/docs')
+api.add_resource(Doc, '/api/docs/<doc_id>')
+api.add_resource(DocsCount, '/api/docs-count')
+api.add_resource(Company, '/api/companies/<company_name>')
+api.add_resource(Companies, '/api/companies')
+api.add_resource(UpdateCompanies, '/api/update/companies')
+
+if __name__ == '__main__':
+    app.run(port='5002')
