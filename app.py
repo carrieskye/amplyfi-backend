e2d088a2a689ddade370e74ade09aca8cdddb14d
Carolyne Peelman <peelmancarolyne@gmail.com>
2019-05-13 21:54:42 +0100

    More info about companies

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse
 from pymongo import MongoClient
+from collections import OrderedDict
 
 load_dotenv()
 
@@ -21,7 +22,7 @@ relations_db = relations_client['amplyfi-relations']
 companies = relations_db.companies
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
 api = Api(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -51,59 +52,82 @@ class DocsCount(Resource):
 
 class Companies(Resource):
     def get(self):
-        return json.loads(json_util.dumps(companies.find().sort("name", 1)))
+        return json.loads(json_util.dumps(companies.find().sort('name', 1)))
 
 
 class Company(Resource):
     def get(self, company_name):
-        company = json.loads(json_util.dumps(companies.find_one({"name": company_name})))
-        company_docs = json.loads(
-            json_util.dumps(docs.find({"m_Companies": {"$in": [company_name]}}).sort('m_szYear', pymongo.DESCENDING)))
-        company['docs'] = company_docs
-        return company
+        # company = json.loads(json_util.dumps(companies.find_one({'name': company_name})))
+        # company_docs = json.loads(
+        #     json_util.dumps(docs.find({'m_Companies': {'$in': [company_name]}}).sort('m_szYear', pymongo.DESCENDING)))
+        # company['docs'] = company_docs
+        # return company
+        return json.loads(json_util.dumps(companies.find_one({'name': company_name})))
 
 
 class UpdateCompanies(Resource):
     def get(self):
-        documents = json.loads(json_util.dumps(docs.find()))
+        documents = json.loads(json_util.dumps(docs.find().limit(200)))
         for doc in documents:
             for company in doc['m_Companies']:
-                if json.loads(json_util.dumps(companies.find_one({"name": company}))) is None:
-                    add_company(company, [doc['m_szDocID']], doc['m_szYear'], doc['m_szYear'])
+                doc_simple = {
+                    'id': doc['_id']['$oid'],
+                    'doc_id': doc['m_szDocID'],
+                    'title': doc['m_szDocTitle'],
+                    'year': doc['m_szYear'],
+                    'location': doc['m_szGeo1'],
+                    'summary': doc['m_szDocSumamry']
+                }
+                if json.loads(json_util.dumps(companies.find_one({'name': company}))) is None:
+                    add_company_with_doc(company, doc_simple['year'], doc_simple['location'], doc_simple,
+                                         doc['m_Companies'])
                 else:
-                    update_company(company, [doc['m_szDocID']], doc['m_szYear'], doc['m_szYear'])
+                    update_company_with_doc(company, doc_simple['year'], doc_simple['location'], doc_simple,
+                                            doc['m_Companies'])
 
         return json.loads(json_util.dumps(companies.find()))
 
 
 def get_document(id):
-    return json.loads(json_util.dumps(docs.find_one({"m_szDocID": id})))
+    return json.loads(json_util.dumps(docs.find_one({'m_szDocID': id})))
 
 
-def add_company(name, docs, min_year, max_year):
+def add_company_with_doc(name, year, location, doc_simple, competitors):
+    competitors.pop(competitors.index(name))
     company = {
-        "name": name,
-        "docs": docs,
-        "min_year": str(min_year),
-        "max_year": str(max_year)
+        'name': name,
+        'years': {year: 1} if year else {},
+        'locations': {location: 1} if location else {},
+        'ids': [doc_simple['id']],
+        'docs': [doc_simple],
+        'competitors': {company.replace('.', ''): 1 for company in competitors}
     }
     companies.insert_one(company)
     return company
 
 
-def update_company(name, new_docs, min_year, max_year):
-    company = json.loads(json_util.dumps(companies.find_one({"name": name})))
+def update_company_with_doc(name, year, location, doc_simple, competitors):
+    company = json.loads(json_util.dumps(companies.find_one({'name': name})))
+    competitors.pop(competitors.index(name))
 
-    for doc in new_docs:
-        if doc not in company['docs']:
-            company['docs'].append(doc)
+    if doc_simple['id'] not in company['ids']:
+        if year:
+            company['years'][year] = company['years'][year] + 1 if year in company['years'] else 1
 
-    company['min_year'] = min_year if min_year < company['min_year'] else company['min_year']
-    company['max_year'] = max_year if max_year > company['max_year'] else company['max_year']
+        if location:
+            company['locations'][location] = company['locations'][location] + 1 if location in company[
+                'locations'] else 1
+
+        for competitor in competitors:
+            company['competitors'][competitor] = company['competitors'][competitor] + 1 if competitor in company[
+                'competitors'] else 1
+
+        company['ids'].append(doc_simple['id'])
+        company['docs'].append(doc_simple)
 
     companies.update_one({'name': name},
-                         {"$set": {'docs': company['docs'], 'min_year': company['min_year'],
-                                   'max_year': company['max_year']}})
+                         {'$set': {'years': company['years'], 'locations': company['locations'], 'ids': company['ids'],
+                                   'docs': company['docs'], 'competitors': company['competitors']}})
 
 
 api.add_resource(Docs, '/api/docs')
